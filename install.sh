115c551800a38373f4cf6ebf05cc431b6b0d1a6f
JooYoung <sana2dang@naver.com>
2017-04-16 23:26:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,13 @@ sudo chmod -R 755 /opt/retropie/configs/all/attractmode/emulators
 sudo chgrp -R -v pi /opt/retropie/configs/all/attractmode/emulators
 sudo chown -R -v pi /opt/retropie/configs/all/attractmode/emulators
 
+sudo cp ./romlist_create.sh /opt/retropie/configs/all/attractmode/Attract Mode Setup
+sudo chmod 755 /opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh 
+sudo chgrp -v pi /opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh
+sudo chown -v pi /opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh
+
 sudo cp ./createRomList /home/pi/dev/
 sudo chmod 755 /home/pi/dev/createRomList 
 sudo chgrp -v pi /home/pi/dev/createRomList 
 sudo chown -v pi /home/pi/dev/createRomList 
+
