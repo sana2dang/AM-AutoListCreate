c757e2d29fd4c90687f0a1da2228a7ff95ac88b6
JooYoung <sana2dang@naver.com>
2017-04-16 23:35:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo chgrp -R -v pi /home/pi/dev/fullRomList
 sudo chown -R -v pi /home/pi/dev/fullRomList
 
 sudo cp -r -v ./emulators /opt/retropie/configs/all/attractmode
-
 sudo chmod -R 755 /opt/retropie/configs/all/attractmode/emulators
 sudo chgrp -R -v pi /opt/retropie/configs/all/attractmode/emulators
 sudo chown -R -v pi /opt/retropie/configs/all/attractmode/emulators
@@ -14,6 +13,12 @@ sudo cp ./romlist_create.sh "/opt/retropie/configs/all/attractmode/Attract Mode 
 sudo chmod 755 "/opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh"
 sudo chgrp -v pi "/opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh"
 sudo chown -v pi "/opt/retropie/configs/all/attractmode/Attract Mode Setup/romlist_create.sh"
+
+sudo cp ./defalut_setting.cfg "/opt/retropie/configs/all/attractmode/"
+sudo chmod 755 "/opt/retropie/configs/all/attractmode/default_setting.cfg"
+sudo chgrp -v pi "/opt/retropie/configs/all/attractmode/default_setting.cfg"
+sudo chown -v pi "/opt/retropie/configs/all/attractmode/default_setting.cfg"
+
 
 sudo cp ./createRomList /home/pi/dev/
 sudo chmod 755 /home/pi/dev/createRomList 
